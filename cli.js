ed9af2462faf285b798f1087456f6f0e967c4552
Floyd Pink <imbleedingme@googlemail.com>
2014-01-30 22:24:45 -0500

    structure for cli is enhanced a bit

--- cli.js
+++ cli.js
@@ -1,12 +1,32 @@
 #!/usr/bin/env node
 'use strict';
 
-var chalk = require('chalk');
+var nopt = require('nopt');
+var pkg = require('./package.json');
+var helper = require('./lib/cli-helper');
 var kollavarsham = require('./lib/kollavarsham');
 
-console.log(chalk.green('Hello, World!'));
-console.log(chalk.green('-------------'));
-console.log();
-console.log(chalk.gray('This next line is coming from within the kollavarsham library'));
-console.log();
-console.log(chalk.blue(kollavarsham.awesome()));
+var opts = nopt({
+  help    : Boolean,
+  version : Boolean
+}, {
+  h : '--help',
+  v : '--version'
+});
+
+var args = opts.argv.remain;
+var cmd = args[0];
+
+helper.openingMessage();
+
+if (opts.help) {
+  helper.helpMessage();
+} else if (opts.version) {
+  return console.log(pkg.version);
+} else {
+  console.log('TODO: Implement the rest...');
+  if (cmd) {
+    console.log('Command: %s', cmd);
+  }
+}
+
