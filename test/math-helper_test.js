ccbafb1edff19a4e02d1d238df313df105269c07
Floyd Pink <imbleedingme@googlemail.com>
2014-02-01 03:08:46 -0500

    some more math functions ported

--- test/math-helper_test.js
+++ test/math-helper_test.js
@@ -22,6 +22,10 @@ var mathHelper = require('../lib/math-helper.js');
     test.ifError(value)
 */
 
+function floatingPointCompare(n1, n2) {
+  return Math.abs(n1 - n2) < 0.0000000001;
+}
+
 exports.kollavarshamHelper = {
   setUp              : function (done) {
     // setup here
@@ -92,6 +96,53 @@ exports.kollavarshamHelper = {
     test.equal(mathHelper.floor('-123456.350'), -123457);
     test.equal(mathHelper.floor(42.999), 42);
     test.equal(mathHelper.floor(42.00001), 42);
+    test.done();
+  },
+  'fractional'       : function (test) {
+    test.expect(10);
+    // tests here
+    test.ok(floatingPointCompare(mathHelper.fractional(0.2345), 0.2345));
+    test.ok(floatingPointCompare(mathHelper.fractional(-0.2385), -0.2385));
+    test.ok(floatingPointCompare(mathHelper.fractional(0.23999), 0.23999));
+    test.ok(floatingPointCompare(mathHelper.fractional(-456.23999), -0.23999));
+    test.ok(floatingPointCompare(mathHelper.fractional(456.999), 0.999));
+    test.ok(floatingPointCompare(mathHelper.fractional('456.999'), 0.999));
+    test.ok(floatingPointCompare(mathHelper.fractional('A quick brown fox'), 0));
+    test.ok(floatingPointCompare(mathHelper.fractional('-123456.350'), -0.350));
+    test.ok(floatingPointCompare(mathHelper.fractional(42.999), 0.999));
+    test.ok(floatingPointCompare(mathHelper.fractional(42.00001), 0.00001));
+    test.done();
+  },
+  'round'            : function (test) {
+    test.expect(11);
+    // tests here
+    test.equal(mathHelper.round(0.2345), 0);
+    test.equal(mathHelper.round(-0.2385), 0);
+    test.equal(mathHelper.round(0.23999), 0);
+    test.equal(mathHelper.round(-456.23999), -456);
+    test.equal(mathHelper.round(456.999), 457);
+    test.equal(mathHelper.round('456.999'), 457);
+    test.equal(mathHelper.round('A quick brown fox'), 0);
+    test.equal(mathHelper.round('-123456.350'), -123456);
+    test.equal(mathHelper.round(42.999), 43);
+    test.equal(mathHelper.round(-42.999), -43);
+    test.equal(mathHelper.round(42.00001), 42);
+    test.done();
+  },
+  'square'           : function (test) {
+    test.expect(11);
+    // tests here
+    test.ok(floatingPointCompare(mathHelper.square(0.2345), 0.05499025));
+    test.ok(floatingPointCompare(mathHelper.square(-0.2385), 0.05688225));
+    test.ok(floatingPointCompare(mathHelper.square(0.23999), 0.0575952001));
+    test.ok(floatingPointCompare(mathHelper.square(-456.23999), 208154.9284752001));
+    test.ok(floatingPointCompare(mathHelper.square(456.999), 208848.086001));
+    test.ok(floatingPointCompare(mathHelper.square('456.999'), 208848.086001));
+    test.ok(floatingPointCompare(mathHelper.square('A quick brown fox'), 0));
+    test.ok(floatingPointCompare(mathHelper.square(42.999), 1848.914001));
+    test.ok(floatingPointCompare(mathHelper.square(42.00001), 1764.0008400001));
+    test.ok(floatingPointCompare(mathHelper.square(5), 25));
+    test.ok(floatingPointCompare(mathHelper.square(9), 81));
     test.done();
   }
 };
