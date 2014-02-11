e4bf69bedb8bc250ee1221d2204746657b525269
Floyd Pink <imbleedingme@googlemail.com>
2014-02-10 23:12:34 -0500

    implemented getSauraMasaName

--- test/calendar_test.js
+++ test/calendar_test.js
@@ -27,36 +27,36 @@ exports.calendar = {
   },
   'nextDate'                     : function (test) {
     test.expect(9);
-    test.ok(cmpDates(cal.nextDate(new Date(2013, cal.months.DECEMBER, 31)), new Date(2014, cal.months.JANUARY, 1)));
-    test.ok(cmpDates(cal.nextDate(new Date(2012, cal.months.JANUARY, 31)), new Date(2012, cal.months.FEBRUARY, 1)));
-    test.ok(cmpDates(cal.nextDate(new Date(2013, cal.months.FEBRUARY, 28)), new Date(2013, cal.months.MARCH, 1)));
-    test.ok(cmpDates(cal.nextDate(new Date(2012, cal.months.FEBRUARY, 28)), new Date(2012, cal.months.FEBRUARY, 29)));
-    test.ok(cmpDates(cal.nextDate(new Date(1950, cal.months.FEBRUARY, 1)), new Date(1950, cal.months.FEBRUARY, 2)));
-    test.ok(cmpDates(cal.nextDate(new Date(1997, cal.months.SEPTEMBER, 30)), new Date(1997, cal.months.OCTOBER, 1)));
-    test.ok(cmpDates(cal.nextDate(new Date(1752, cal.months.MARCH, 24)), new Date(1752, cal.months.MARCH, 25)));
-    test.ok(cmpDates(cal.nextDate(new Date(1752, cal.months.SEPTEMBER, 2)), new Date(1752, cal.months.SEPTEMBER, 3)));
-    test.ok(cmpDates(cal.nextDate(new Date(1997, cal.months.DECEMBER, 30)), new Date(1997, cal.months.DECEMBER, 31)));
+    test.ok(cmpDates(cal.nextDate(new Date(2013, cal.months.December, 31)), new Date(2014, cal.months.January, 1)));
+    test.ok(cmpDates(cal.nextDate(new Date(2012, cal.months.January, 31)), new Date(2012, cal.months.February, 1)));
+    test.ok(cmpDates(cal.nextDate(new Date(2013, cal.months.February, 28)), new Date(2013, cal.months.March, 1)));
+    test.ok(cmpDates(cal.nextDate(new Date(2012, cal.months.February, 28)), new Date(2012, cal.months.February, 29)));
+    test.ok(cmpDates(cal.nextDate(new Date(1950, cal.months.February, 1)), new Date(1950, cal.months.February, 2)));
+    test.ok(cmpDates(cal.nextDate(new Date(1997, cal.months.September, 30)), new Date(1997, cal.months.October, 1)));
+    test.ok(cmpDates(cal.nextDate(new Date(1752, cal.months.March, 24)), new Date(1752, cal.months.March, 25)));
+    test.ok(cmpDates(cal.nextDate(new Date(1752, cal.months.September, 2)), new Date(1752, cal.months.September, 3)));
+    test.ok(cmpDates(cal.nextDate(new Date(1997, cal.months.December, 30)), new Date(1997, cal.months.December, 31)));
     test.done();
   },
   'gregorianDateToJulianDay'     : function (test) {
     test.expect(14);
-    test.equal(cal.gregorianDateToJulianDay(new Date(2013, cal.months.DECEMBER, 30)), 2456656.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(2013, cal.months.DECEMBER, 31)), 2456657.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(2012, cal.months.JANUARY, 31)), 2455957.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(2013, cal.months.FEBRUARY, 28)), 2456351.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(2012, cal.months.FEBRUARY, 28)), 2455985.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(2001, cal.months.JANUARY, 1)), 2451910.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(1950, cal.months.FEBRUARY, 1)), 2433313.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(1997, cal.months.SEPTEMBER, 30)), 2450721.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(1752, cal.months.MARCH, 24)), 2361047.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(1752, cal.months.SEPTEMBER, 2)), 2361209.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(1997, cal.months.DECEMBER, 30)), 2450812.5);
-    test.equal(cal.gregorianDateToJulianDay(new Date(-1, cal.months.JANUARY, 31)), 1720722.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(2013, cal.months.December, 30)), 2456656.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(2013, cal.months.December, 31)), 2456657.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(2012, cal.months.January, 31)), 2455957.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(2013, cal.months.February, 28)), 2456351.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(2012, cal.months.February, 28)), 2455985.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(2001, cal.months.January, 1)), 2451910.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(1950, cal.months.February, 1)), 2433313.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(1997, cal.months.September, 30)), 2450721.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(1752, cal.months.March, 24)), 2361047.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(1752, cal.months.September, 2)), 2361209.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(1997, cal.months.December, 30)), 2450812.5);
+    test.equal(cal.gregorianDateToJulianDay(new Date(-1, cal.months.January, 31)), 1720722.5);
     // Special case for setting dates from years 0 - 99 AD
-    var dateFrom7AD = new Date(7, cal.months.JANUARY, 1);
+    var dateFrom7AD = new Date(7, cal.months.January, 1);
     dateFrom7AD.setFullYear(7);
     test.equal(cal.gregorianDateToJulianDay(dateFrom7AD), 1723614.5);
-    var dateFrom0AD = new Date(0, cal.months.JANUARY, 1);
+    var dateFrom0AD = new Date(0, cal.months.January, 1);
     dateFrom0AD.setFullYear(0);
     test.equal(cal.gregorianDateToJulianDay(dateFrom0AD), 1721057.5);
     test.done();
@@ -74,17 +74,17 @@ exports.calendar = {
   },
   'julianInEnglandFromGregorian' : function (test) {
     test.expect(11);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(200, cal.months.JANUARY, 31))), false);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.OCTOBER, 12))), false);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.OCTOBER, 13))), false);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.OCTOBER, 14))), false);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.OCTOBER, 15))), true);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.OCTOBER, 16))), true);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.SEPTEMBER, 11))), true);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.SEPTEMBER, 12))), true);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.SEPTEMBER, 13))), true);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.SEPTEMBER, 14))), false);
-    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.SEPTEMBER, 15))), false);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(200, cal.months.January, 31))), false);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.October, 12))), false);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.October, 13))), false);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.October, 14))), false);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.October, 15))), true);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1582, cal.months.October, 16))), true);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.September, 11))), true);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.September, 12))), true);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.September, 13))), true);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.September, 14))), false);
+    test.equal(cal.julianInEngland(cal.gregorianDateToJulianDay(new Date(1752, cal.months.September, 15))), false);
     test.done();
   },
   'julianDayToJulianDate'        : function (test) {
@@ -101,22 +101,22 @@ exports.calendar = {
   },
   'julianDayToGregorianDate'     : function (test) {
     test.expect(14);
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2299158.5), new Date(1582, cal.months.OCTOBER, 13)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2299159.5), new Date(1582, cal.months.OCTOBER, 14)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2299160.5), new Date(1582, cal.months.OCTOBER, 15)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2299161.5), new Date(1582, cal.months.OCTOBER, 16)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2361220.5), new Date(1752, cal.months.SEPTEMBER, 13)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2361221.5), new Date(1752, cal.months.SEPTEMBER, 14)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2361222.5), new Date(1752, cal.months.SEPTEMBER, 15)));
-    var dateFrom1AD = new Date(1, cal.months.FEBRUARY, 2);
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2299158.5), new Date(1582, cal.months.October, 13)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2299159.5), new Date(1582, cal.months.October, 14)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2299160.5), new Date(1582, cal.months.October, 15)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2299161.5), new Date(1582, cal.months.October, 16)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2361220.5), new Date(1752, cal.months.September, 13)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2361221.5), new Date(1752, cal.months.September, 14)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2361222.5), new Date(1752, cal.months.September, 15)));
+    var dateFrom1AD = new Date(1, cal.months.February, 2);
     dateFrom1AD.setFullYear(1);
     test.ok(cmpDates(cal.julianDayToGregorianDate(1721457.5), dateFrom1AD));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2456656.5), new Date(2013, cal.months.DECEMBER, 30)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2456657.5), new Date(2013, cal.months.DECEMBER, 31)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2455957.5), new Date(2012, cal.months.JANUARY, 31)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2456351.5), new Date(2013, cal.months.FEBRUARY, 28)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2455985.5), new Date(2012, cal.months.FEBRUARY, 28)));
-    test.ok(cmpDates(cal.julianDayToGregorianDate(2433313.5), new Date(1950, cal.months.FEBRUARY, 1)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2456656.5), new Date(2013, cal.months.December, 30)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2456657.5), new Date(2013, cal.months.December, 31)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2455957.5), new Date(2012, cal.months.January, 31)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2456351.5), new Date(2013, cal.months.February, 28)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2455985.5), new Date(2012, cal.months.February, 28)));
+    test.ok(cmpDates(cal.julianDayToGregorianDate(2433313.5), new Date(1950, cal.months.February, 1)));
     test.done();
   },
   'julianDayToModernDate'        : function (test) {
@@ -126,15 +126,15 @@ exports.calendar = {
     test.equal(cal.julianDayToModernDate(2299160.5).toString(), '1582 10 04');
     test.equal(cal.julianDayToModernDate(2299161.5).toString(), '1582 10 05');
     test.equal(cal.julianDayToModernDate(1721457.5).toString(), '0001 02 03');
-    test.ok(cmpDates(cal.julianDayToModernDate(2361220.5), new Date(1752, cal.months.SEPTEMBER, 13)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2361221.5), new Date(1752, cal.months.SEPTEMBER, 14)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2361222.5), new Date(1752, cal.months.SEPTEMBER, 15)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2456656.5), new Date(2013, cal.months.DECEMBER, 30)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2456657.5), new Date(2013, cal.months.DECEMBER, 31)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2455957.5), new Date(2012, cal.months.JANUARY, 31)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2456351.5), new Date(2013, cal.months.FEBRUARY, 28)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2455985.5), new Date(2012, cal.months.FEBRUARY, 28)));
-    test.ok(cmpDates(cal.julianDayToModernDate(2433313.5), new Date(1950, cal.months.FEBRUARY, 1)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2361220.5), new Date(1752, cal.months.September, 13)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2361221.5), new Date(1752, cal.months.September, 14)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2361222.5), new Date(1752, cal.months.September, 15)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2456656.5), new Date(2013, cal.months.December, 30)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2456657.5), new Date(2013, cal.months.December, 31)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2455957.5), new Date(2012, cal.months.January, 31)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2456351.5), new Date(2013, cal.months.February, 28)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2455985.5), new Date(2012, cal.months.February, 28)));
+    test.ok(cmpDates(cal.julianDayToModernDate(2433313.5), new Date(1950, cal.months.February, 1)));
     test.done();
   },
   'julianDayToAhargana'          : function (test) {
@@ -248,6 +248,38 @@ exports.calendar = {
     test.ok(math.floatingPointEqual(cal.findSamkranti(1868266, 1868267), 1868266.00000001));
     test.ok(math.floatingPointEqual(cal.findSamkranti(1721431, 1721432), 1721431.9425787));
     test.ok(math.floatingPointEqual(cal.findSamkranti(2299153, 2299154), 2299153.23922039));
+    test.done();
+  },
+  'getMasaName'                  : function (test) {
+    test.expect(12);
+    test.equal(cal.getMasaName(0), 'Caitra    ');
+    test.equal(cal.getMasaName(1), 'Vaisakha  ');
+    test.equal(cal.getMasaName(2), 'Jyaistha  ');
+    test.equal(cal.getMasaName(3), 'Asadha    ');
+    test.equal(cal.getMasaName(4), 'Sravana   ');
+    test.equal(cal.getMasaName(5), 'Bhadrapada');
+    test.equal(cal.getMasaName(6), 'Asvina    ');
+    test.equal(cal.getMasaName(7), 'Karttika  ');
+    test.equal(cal.getMasaName(8), 'Margasirsa');
+    test.equal(cal.getMasaName(9), 'Pausa     ');
+    test.equal(cal.getMasaName(10), 'Magha     ');
+    test.equal(cal.getMasaName(11), 'Phalguna  ');
+    test.done();
+  },
+  'getSauraMasaName'             : function (test) {
+    test.expect(12);
+    test.equal(cal.getSauraMasaName(0), 'Mesa   ');
+    test.equal(cal.getSauraMasaName(1), 'Vrsa   ');
+    test.equal(cal.getSauraMasaName(2), 'Mithuna');
+    test.equal(cal.getSauraMasaName(3), 'Karkata');
+    test.equal(cal.getSauraMasaName(4), 'Simha  ');
+    test.equal(cal.getSauraMasaName(5), 'Kanya  ');
+    test.equal(cal.getSauraMasaName(6), 'Tula   ');
+    test.equal(cal.getSauraMasaName(7), 'Vrscika');
+    test.equal(cal.getSauraMasaName(8), 'Dhanus ');
+    test.equal(cal.getSauraMasaName(9), 'Makara ');
+    test.equal(cal.getSauraMasaName(10), 'Kumbha ');
+    test.equal(cal.getSauraMasaName(11), 'Mina   ');
     test.done();
   }
 };
