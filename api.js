235fa92c4de1fc2e0faffc8644a7bfa75eea25a8
Floyd Pink <imbleedingme@googlemail.com>
2014-02-13 02:05:31 -0500

    Built kollavarsham API documentation from commit 0f47b14 on branch master

---
+++ api.js
@@ -0,0 +1,66 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "JulianDate",
+        "calendar",
+        "cliHelper",
+        "globals",
+        "locations",
+        "math"
+    ],
+    "modules": [
+        "JulianDate",
+        "calculations",
+        "calendar",
+        "cli",
+        "cli-helper",
+        "globals",
+        "kollavrsham",
+        "locations",
+        "math"
+    ],
+    "allModules": [
+        {
+            "displayName": "calculations",
+            "name": "calculations",
+            "description": "Calculations involved in the to and fro conversions of Kollavarsham to Gregorian dates and vice versa"
+        },
+        {
+            "displayName": "calendar",
+            "name": "calendar"
+        },
+        {
+            "displayName": "cli",
+            "name": "cli",
+            "description": "CLI app to convert Gregorian date to Kollavarsham date and vice versa"
+        },
+        {
+            "displayName": "cli-helper",
+            "name": "cli-helper",
+            "description": "Helper functions for the CLI app"
+        },
+        {
+            "displayName": "globals",
+            "name": "globals",
+            "description": "Temporary module that holds all the Perl global variables"
+        },
+        {
+            "displayName": "JulianDate",
+            "name": "JulianDate"
+        },
+        {
+            "displayName": "kollavrsham",
+            "name": "kollavrsham",
+            "description": "CLI app to convert Gregorian date to Kollavarsham date and vice versa"
+        },
+        {
+            "displayName": "locations",
+            "name": "locations"
+        },
+        {
+            "displayName": "math",
+            "name": "math"
+        }
+    ]
+} };
+});
