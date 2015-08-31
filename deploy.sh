2373bc9a514e08735267f71eec836a9d6deb02da
Floyd Pink <imbleedingme@googlemail.com>
2015-08-31 14:17:16 -0400

    stop pushing code coverage to coveralls from travis

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -e
 grunt buildcontrol:pages
-npm install -g istanbul
-istanbul cover node_modules/grunt-contrib-nodeunit/node_modules/nodeunit/bin/nodeunit -- test
-npm install -g codeclimate-test-reporter
 codeclimate-test-reporter < ./coverage/lcov.info
-npm install coveralls --save
-cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
